caa4700c61431d703239b035bda7f439c8451d6e
DICT2-1-RestaurantManagementSystem <140129598+DICT2-1-RestaurantManagementSystem@users.noreply.github.com>
2023-07-21 09:50:27 +0800

    Update login.java

--- RestaurantManagement_System/src/rms_dashboard/login.java
+++ RestaurantManagement_System/src/rms_dashboard/login.java
@@ -94,3 +94,124 @@ public class login extends JFrame {
         });
     }
 }
+
+/*
+package rms_dashboard;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class login extends JFrame {
+    private JTextField usernameField;
+    private JPasswordField passwordField;
+    private Connection connection;
+
+    public login() {
+        setTitle("Login Page");
+        setSize(400, 250); // Increase height to accommodate the new label
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        // Establish the database connection
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:users.db");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Failed to connect to the database.");
+            System.exit(1);
+        }
+
+        JPanel panel = new JPanel(new GridBagLayout());
+        panel.setBackground(Color.WHITE); // Set background color to white
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5); // Add some padding
+
+        JLabel loginLabel = new JLabel("Log In");
+        loginLabel.setFont(new Font("Arial", Font.BOLD, 24)); // Set font size and style
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2; // Span two columns
+        panel.add(loginLabel, gbc);
+
+        JLabel usernameLabel = new JLabel("Username:");
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 1; // Reset gridwidth to 1
+        panel.add(usernameLabel, gbc);
+
+        usernameField = new JTextField(15); // Set preferred width
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        panel.add(usernameField, gbc);
+
+        JLabel passwordLabel = new JLabel("Password:");
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        panel.add(passwordLabel, gbc);
+
+        passwordField = new JPasswordField(15); // Set preferred width
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        panel.add(passwordField, gbc);
+
+        JButton loginButton = new JButton("Login");
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        gbc.gridwidth = 2; // Make the button span two columns
+        gbc.fill = GridBagConstraints.HORIZONTAL; // Stretch the button horizontally
+        panel.add(loginButton, gbc);
+
+        loginButton.setBackground(Color.GREEN); // Set the button color to green
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = usernameField.getText();
+                char[] passwordChars = passwordField.getPassword();
+                String password = new String(passwordChars);
+
+                if (isValidCredentials(username, password)) {
+                    // If login is successful, close the login page and open the Menu page
+                    Menu menu = new Menu();
+                    menu.setVisible(true);
+                    dispose(); // This will close the login page
+                } else {
+                    JOptionPane.showMessageDialog(null, "Invalid credentials. Please try again.");
+                }
+            }
+        });
+
+        add(panel);
+    }
+
+    private boolean isValidCredentials(String username, String password) {
+        String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+            ResultSet resultSet = stmt.executeQuery();
+            return resultSet.next(); // If a matching user is found, the login is successful
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                login loginPage = new login();
+                loginPage.setVisible(true);
+            }
+        });
+    }
+}
+*/
